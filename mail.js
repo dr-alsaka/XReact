005a1cd6d13a0ad346d2df467318ebfc6f8f5c66
Ahmed El-saka <69402091+Sakaa@users.noreply.github.com>
2021-08-27 15:13:30 +0200

    Delete mail.js

--- mail.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-var mailOptions = {
-    from: 'ahmed@gmail.com',
-    to: '.com',
-    subject: 'Sending Email using Node.js',
-    html: '<h1>Welcome</h1><p>That was easy!</p>'
-  }
-  
