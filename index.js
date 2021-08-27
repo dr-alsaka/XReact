dc96b797ec78d72df8c62027cc0672ad0160c906
Ahmed El-saka <69402091+Sakaa@users.noreply.github.com>
2021-08-27 15:05:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const mongo = require('mongodb');
 http.createServer((req, res) => {
-  fs.readFile('files/'+req.url,(err, data) => {
+  fs.readFile('fiels/'+req.url,(err, data) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
